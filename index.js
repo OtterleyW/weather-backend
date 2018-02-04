196e2362e3a18fcc3847135e36172fba821afda8
Jenni Nurmi <jenniknurmi@gmail.com>
2018-02-04 12:21:11 +0200

    Testaa tietokantaa

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ const db = require('./queries');
 app.use(cors());
 app.use(express.static('build'))
 
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
