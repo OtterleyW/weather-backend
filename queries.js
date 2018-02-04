8b9ef8ac4c2fddade4a90ee5fedf50f8495ff7c1
Jenni Nurmi <jenniknurmi@gmail.com>
2018-02-04 13:03:08 +0200

    Lisää SSL productionin tietokantayhteyteen

--- queries.js
+++ queries.js
@@ -1,66 +1,73 @@
-var promise = require('bluebird');
+var promise = require("bluebird");
 
 var options = {
   // Initialization Options
   promiseLib: promise
 };
 
-if ( process.env.NODE_ENV !== 'production' ) {
-  require('dotenv').config()
+var connectionString = "";
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+  connectionString = process.env.DATABASE_URL;
+} else {
+  connectionString = process.env.DATABASE_URL + "?ssl=true";
 }
 
-var pgp = require('pg-promise')(options);
-var connectionString = process.env.DATABASE_URL+'?ssl=true';
+var pgp = require("pg-promise")(options);
+console.log("Database", process.env.DATABASE_URL);
 var db = pgp(connectionString);
 
 function getAllCities(req, res) {
-  db.any('select * from cities')
-    .then(function (data) {
-      res.status(200)
-        .json({data
-        });
+  db
+    .any("select * from cities")
+    .then(function(data) {
+      res.status(200).json({
+        data
+      });
     })
     .catch(error => {
-      console.log(error)
-      response.status(404).end()
-    })
+      console.log(error);
+      response.status(404).end();
+    });
 }
 
 function getSingleCity(req, res) {
   var cityID = parseInt(req.params.id);
-  db.one('select * from cities where id = $1', cityID)
-    .then(function (data) {
-      res.status(200)
-        .json({
-          status: 'success',
-          data: data,
-          message: 'Retrieved ONEcity'
-        });
+  db
+    .one("select * from cities where id = $1", cityID)
+    .then(function(data) {
+      res.status(200).json({
+        status: "success",
+        data: data,
+        message: "Retrieved ONEcity"
+      });
     })
     .catch(error => {
-      console.log(error)
-      response.status(404).end()
-    })
+      console.log(error);
+      response.status(404).end();
+    });
 }
 
 function getPerceptionsOfOneCity(req, res) {
   var cityID = parseInt(req.params.id);
-  db.any('select * from cities INNER JOIN perceptions ON cities.id = perceptions.city_id WHERE cities.id = $1', cityID).then(function(data) {
-    res.status(200).json({
-      data: data
+  db
+    .any(
+      "select * from cities INNER JOIN perceptions ON cities.id = perceptions.city_id WHERE cities.id = $1",
+      cityID
+    )
+    .then(function(data) {
+      res.status(200).json({
+        data: data
+      });
     })
-  }).catch(error => {
-    console.log(error)
-    response.status(404).end()
-  })
+    .catch(error => {
+      console.log(error);
+      response.status(404).end();
+    });
 }
-
 
 module.exports = {
   getAllCities: getAllCities,
   getSingleCity: getSingleCity,
   getPerceptionsOfOneCity: getPerceptionsOfOneCity
 };
-
-
-
