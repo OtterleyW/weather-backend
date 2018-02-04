16c5c8f87edc6534d00a701454760afaff169220
Jenni Nurmi <jenniknurmi@gmail.com>
2018-02-04 12:05:42 +0200

    Luo Herokuun tietokantayhteys

--- queries.js
+++ queries.js
@@ -6,7 +6,7 @@ var options = {
 };
 
 var pgp = require('pg-promise')(options);
-var connectionString = 'postgres://localhost:5432/weather';
+var connectionString = process.env.DATABASE_URL;
 var db = pgp(connectionString);
 
 function getAllCities(req, res) {
