8cac9d0fb6bb4890b8ff993472ccc05a6c10322d
Jenni Nurmi <jenniknurmi@gmail.com>
2018-02-04 11:33:43 +0200

    Yhdistä frontend ja backend

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get("/api/cities/:id/perceptions", (request, response) => {
   db.getPerceptionsOfOneCity(request, response);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
