1ed87ec205ae76dd899ca7fa2409b3a52fa209f7
Jenni Nurmi <jenniknurmi@gmail.com>
2018-02-07 19:15:35 +0200

    Korjaa tietokannan esimerkkidatasta pari hassuutta

--- weather.sql
+++ weather.sql
@@ -13,13 +13,12 @@ CREATE TABLE perceptions (
   ID SERIAL PRIMARY KEY,
   city_id INTEGER,
   temperature NUMERIC,
-  comment VARCHAR,
   created_at TIMESTAMP WITH TIME ZONE
 );
 
 -- Insert given observation locations to database.
 INSERT INTO cities (name, location)
-  VALUES ('Tokio', '35.6584421,139.7328635');
+  VALUES ('Tokyo', '35.6584421,139.7328635');
   INSERT INTO cities (name, location)
   VALUES ('Helsinki', '60.1697530,24.9490830');
   INSERT INTO cities (name, location)
@@ -30,12 +29,12 @@ INSERT INTO cities (name, location)
   VALUES ('Dubai', '25.092535,55.1562243');
 
 -- Insert some exampledata for observations in different locations
-INSERT INTO perceptions (city_id, temperature, comment, created_at) VALUES (1, 15, 'Windy', now());
+INSERT INTO perceptions (city_id, temperature, created_at) VALUES (1, 15, now());
 
-INSERT INTO perceptions (city_id, temperature, comment, created_at) VALUES (2, -5, 'Snowing', now());
+INSERT INTO perceptions (city_id, temperature, created_at) VALUES (2, -5, now());
 
-INSERT INTO perceptions (city_id, temperature, comment, created_at) VALUES (3, 40, 'Windy', now());
+INSERT INTO perceptions (city_id, temperature, created_at) VALUES (3, 10, now());
 
-INSERT INTO perceptions (city_id, temperature, comment, created_at) VALUES (4, 5, 'Raining', now());
+INSERT INTO perceptions (city_id, temperature, created_at) VALUES (4, 5, now());
 
-INSERT INTO perceptions (city_id, temperature, comment, created_at) VALUES (5, 30, 'Sunny', now());
+INSERT INTO perceptions (city_id, temperature, created_at) VALUES (5, 30, now());
